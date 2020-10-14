e9246e0d1e60e71d5e15c3c22035d994056217b3
sheikhrachel <sheikhisaac@gmail.com>
2020-10-13 18:39:37 -0700

    housecleaning

--- cmd/coldstart.go
+++ cmd/coldstart.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -22,7 +7,6 @@ import (
 	"fmt"
 )
 
-// coldstartCmd represents the coldstart command
 var coldstartCmd = &cobra.Command{
 	Use:   "coldstart",
 	Short: "Project Generation",
@@ -62,15 +46,9 @@ Supported licenses: apache, gnugpl3, mit
 
 func init() {
 	rootCmd.AddCommand(coldstartCmd)
-
-	// Here you will define your flags and configuration settings.
-
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	//coldstartCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	coldstartCmd.Flags().StringP("name", "n", "", "Set the project name")
 	coldstartCmd.Flags().StringP("language", "l", "", "Set the codebase language")
 	coldstartCmd.Flags().StringP("license", "x", "", "Set the license")
@@ -82,9 +60,6 @@ func create_project(name string) {
 	os.Chdir(name)
 	wd, _ := os.Getwd()
     create_readme(wd, name)
-
-    // s, _ := os.Getwd()
-    // fmt.Println(s)
 }
 
 func create_readme(directory string, name string) {
